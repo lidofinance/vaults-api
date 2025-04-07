8baa2d1cf744027fd141e1b1f9da1f7ef7ef6345
Anton Shalimov <a.shalimof@yandex.ru>
2025-04-07 12:19:02 +0300

    feat: add simple example

--- src/common/contracts/contracts.module.ts
+++ src/common/contracts/contracts.module.ts
@@ -1,5 +1,6 @@
 import { LidoContractModule, LidoLocatorContractModule } from '@lido-nestjs/contracts';
 import { Global, Module } from '@nestjs/common';
+import { getDefaultProvider } from '@ethersproject/providers';
 import { ConfigService } from '../config';
 
 @Global()
@@ -7,9 +8,12 @@ import { ConfigService } from '../config';
   imports: [LidoContractModule, LidoLocatorContractModule].map((module) =>
     module.forRootAsync({
       async useFactory(config: ConfigService) {
+        // TODO: provider will be changed later (this one is used for testing only PoC)
+        const provider = getDefaultProvider('mainnet');
+
         const addressMap = await config.getCustomConfigContractsAddressMap();
         const address = addressMap ? addressMap.get(module.contractToken) : undefined;
-        return { address };
+        return { provider, address };
       },
       inject: [ConfigService],
     }),
