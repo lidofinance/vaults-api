14ff201d4ca7e456eebf92f5b68e309a37414aa2
Anton Shalimov <a.shalimof@yandex.ru>
2025-04-24 22:13:16 +0300

    feat: add the 'validator index is out of range' case

--- src/http/proof/proof.controller.ts
+++ src/http/proof/proof.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Param, Version } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
-import { LsvService } from 'lsv/lsv.service';
+import { BadRequestException } from '@nestjs/common';
+
+import { LsvService, VALIDATOR_INDEX_IS_OUT_OF_RANGE_ERROR } from 'lsv/lsv.service';
 import { Proof } from './entities';
 
 @Controller('proof')
@@ -17,6 +19,11 @@ export class ProofController {
   })
   async create(@Param('validatorIndex') validatorIndex: number) {
     const proof = await this.lsvService.createProof(validatorIndex);
+
+    if (proof === VALIDATOR_INDEX_IS_OUT_OF_RANGE_ERROR) {
+      throw new BadRequestException(`Validator index ${validatorIndex} is out of range`);
+    }
+
     // TODO: to be changed in future PRs
     return JSON.parse(JSON.stringify(proof, (_, value) => (typeof value === 'bigint' ? value.toString() : value)));
   }
--- src/lsv/lsv.service.ts
+++ src/lsv/lsv.service.ts
@@ -3,6 +3,8 @@ import { createPDGProof } from '@lidofinance/lsv-cli/dist/utils';
 
 import { ConfigService } from '../common/config';
 
+export const VALIDATOR_INDEX_IS_OUT_OF_RANGE_ERROR = 'VALIDATOR_INDEX_IS_OUT_OF_RANGE_ERROR';
+
 @Injectable()
 export class LsvService {
   constructor(protected readonly configService: ConfigService) {}
@@ -11,7 +13,12 @@ export class LsvService {
     try {
       return await createPDGProof(validatorIndex, this.configService.get('CL_URL'));
     } catch (error) {
-      console.error(`Failed to create PDG proof for validatorIndex ${validatorIndex}:`, error);
+      if (error instanceof Error && error.message.startsWith(`ValidatorIndex ${validatorIndex} out of range`)) {
+        console.warn(`[LsvService.createProof] Validator index ${validatorIndex} is out of range`);
+        return VALIDATOR_INDEX_IS_OUT_OF_RANGE_ERROR;
+      }
+
+      console.error(`[LsvService.createProof] Failed to create PDG proof for validatorIndex ${validatorIndex}:`, error);
       throw error;
     }
   }
