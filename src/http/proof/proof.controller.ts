6cc899224695337cf31484f28b1b444f9e3b20a0
Anton Shalimov <a.shalimof@yandex.ru>
2025-04-30 13:16:24 +0300

    feat: hide the 'v1/proof/make/<validatorIndex>' endpoint

--- src/http/proof/proof.controller.ts
+++ src/http/proof/proof.controller.ts
@@ -1,11 +1,12 @@
 import { plainToInstance } from 'class-transformer';
 import { Controller, Get, Param, Version, ClassSerializerInterceptor, UseInterceptors } from '@nestjs/common';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiExcludeController, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { BadRequestException } from '@nestjs/common';
 
 import { LsvService, VALIDATOR_INDEX_IS_OUT_OF_RANGE_ERROR } from 'lsv/lsv.service';
 import { ProofDto } from './dto';
 
+@ApiExcludeController()
 @Controller('proof')
 @ApiTags('Proof')
 @UseInterceptors(ClassSerializerInterceptor)
