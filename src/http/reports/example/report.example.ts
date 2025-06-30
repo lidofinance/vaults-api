09edfc69c0b93eb0da6a364cb84e3b8f539d4800
Anton Shalimov <a.shalimof@yandex.ru>
2025-06-30 16:12:14 +0300

    chore: updated reportByVaultExample

--- src/http/reports/example/report.example.ts
+++ src/http/reports/example/report.example.ts
@@ -1,16 +1,18 @@
 export const reportByVaultExample = {
-  data: {
-    vault_address: '0xE312f1ed35c4dBd010A332118baAD69d45A0E302',
-    in_out_delta: '33000000000000000000',
-    fee: '0',
-    total_value_wei: '33000000000000000000',
-    liability_shares: '0',
+  "data": {
+    "vaultAddress": "0x7228FC874C1D08cAE68a558d7B650fc4862B1DB7",
+    "fee": "761489401252193",
+    "totalValueWei": "65044706059226272543",
+    "liabilityShares": "3000000000000000000",
+    "slashingReserve": "0"
   },
-  leaf: '0x984bde8d59aca15d6f5ff9c21de604cca0fd031c7f2b1cecf9c0a8912d627660',
-  refSlot: 317279,
-  blockNumber: 295607,
-  timestamp: 1746020748,
-  proofsCID: 'QmYR5S97ahBrxWub21kAc5xrXSCDr7ZKy3RGNihehXE3tn',
-  merkleTreeRoot: '0xd0c27941e1a2663a0eeb56df4b2f7569a1c360b16c65ac35634c9ae03ec2644a',
-  prevTreeCID: 'QmNxZrnCiYeXdGrey9rQCrtzHtHXCNg25MiVcZQJnEqjdd',
+  "extraData": {
+    "inOutDelta": "65019709677226272543"
+  },
+  "leaf": "0x6ee9644cfdb8d474df648a9c0a3a01a95f626c837fa12c718b06b92774f4ab22",
+  "refSlot": 755903,
+  "blockNumber": 708049,
+  "timestamp": 1751284236,
+  "merkleTreeRoot": "0x417a8edd3729c2455e82f97f3037331b4b89d41db607cb65a251b35f6c61c0c7",
+  "prevTreeCID": "QmYkkzjuNQpwYDsQpUjitUZy6XAe52Ywbc4K2VAKHLKdyE"
 };
