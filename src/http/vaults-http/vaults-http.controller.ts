4c39bdcf0dc357573cceed518b1a48b082c6ba44
Anton Shalimov <a.shalimof@yandex.ru>
2025-07-01 13:48:07 +0300

    feat(api): add GET :vaultAddress/metrics-range

--- src/http/vaults-http/vaults-http.controller.ts
+++ src/http/vaults-http/vaults-http.controller.ts
@@ -19,6 +19,7 @@ import { ConfigService } from 'common/config';
 import { VaultDbService, SortFieldsEnum, DirectionEnum } from 'db/vault-db';
 import { ALL_ROLE_VALUES } from 'vault/vault.constants';
 
+import { GetVaultStatsRangeQueryDto } from './dto/get-vault-stats-range-query.dto';
 import { vaultsExample, vaultLatestMetricsExample } from './example';
 
 const limitQueryDefault = 10;
@@ -131,6 +132,43 @@ export class VaultsHttpController {
     }
 
     return latestStats;
+  }
+
+  @Version('1')
+  @Get(':vaultAddress/metrics-range')
+  @ApiParam({ name: 'vaultAddress', type: String, description: 'Vault address (0x...)' })
+  @ApiQuery({ name: 'fromTimestamp', required: false, type: Number })
+  @ApiQuery({ name: 'toTimestamp', required: false, type: Number })
+  @ApiQuery({ name: 'fromBlock', required: false, type: Number })
+  @ApiQuery({ name: 'toBlock', required: false, type: Number })
+  @ApiResponse({
+    status: 200,
+    description: 'Vaults with latest metrics',
+    schema: {
+      example: [vaultLatestMetricsExample, vaultLatestMetricsExample],
+    },
+  })
+  async getVaultStatsRangeByAddress(
+    @Param('vaultAddress') vaultAddress: string,
+    @Query() query: GetVaultStatsRangeQueryDto,
+  ) {
+    const { fromBlock, toBlock, fromTimestamp, toTimestamp } = query;
+
+    const hasBlockRange = fromBlock !== undefined && toBlock !== undefined;
+    const hasTimestampRange = fromTimestamp !== undefined && toTimestamp !== undefined;
+
+    if (!hasBlockRange && !hasTimestampRange) {
+      throw new BadRequestException(
+        'You must provide either both "fromBlock" & "toBlock" or both "fromTimestamp" & "toTimestamp".',
+      );
+    }
+
+    if (hasBlockRange) {
+      return this.vaultDbService.getVaultReportStatsInRange(vaultAddress, undefined, undefined, fromBlock, toBlock);
+    }
+
+    // by timestamps
+    return this.vaultDbService.getVaultReportStatsInRange(vaultAddress, fromTimestamp, toTimestamp);
   }
 
   private getNextVaultsHourlyUpdate(): Date {
