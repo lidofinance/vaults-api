f139a9fac0c827e92639b90209c7f6bf29cb25ef
Anton Shalimov <a.shalimof@yandex.ru>
2025-07-02 16:37:45 +0300

    feat(vaults http): add bad request exceptions

--- src/http/common/dto/error-response-type.ts
+++ src/http/common/dto/error-response-type.ts
@@ -2,19 +2,19 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class ErrorResponseType {
   @ApiProperty({
-    example: 400,
-    description: 'Http status code',
-  })
-  statusCode: number;
-
-  @ApiProperty({
     description: 'Array of validation error messages',
   })
-  message: string[];
+  message: string;
 
   @ApiProperty({
     description: 'Error text',
     example: 'Bad Request',
   })
   error: string;
+
+  @ApiProperty({
+    example: 400,
+    description: 'Http status code',
+  })
+  statusCode: number;
 }
