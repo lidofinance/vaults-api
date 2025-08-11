2e4b8c704f2d10f0162f7fdc83e0f3035eaf6ce4
Anton Shalimov <a.shalimof@yandex.ru>
2025-08-11 13:21:37 +0300

    feat(httpRequestDuration): add {url args} in the metric

--- src/http/http.constants.ts
+++ src/http/http.constants.ts
@@ -1,6 +1,13 @@
 export const HTTP_PATHS = {
   1: {
     'proof/make': 'proof/make',
-    'report/last': 'report/last',
+
+    'report/{cid}/{vaultAddress}': 'report/{cid}/{vaultAddress}',
+    'report/last/{vaultAddress}': 'report/last/{vaultAddress}',
+    'report/previous/{vaultAddress}': 'report/previous/{vaultAddress}',
+
+    vaults: 'vaults',
+    'vaults/{vaultAddress}/latest-metrics': 'vaults/{vaultAddress}/latest-metrics',
+    'vaults/{vaultAddress}/metrics-range': 'vaults/{vaultAddress}/metrics-range',
   },
-};
+} as const;
