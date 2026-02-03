6a19c0184cd2be6b29722ee3574c88764a8a2529
Anton Shalimov <a.shalimof@yandex.ru>
2026-02-03 22:55:14 +0300

    test: add e2e tests for ReportsHttpController.getLast

--- src/http/reports-http/tests/get-last.e2e-spec.ts
+++ src/http/reports-http/tests/get-last.e2e-spec.ts
@@ -4,23 +4,32 @@ import { INestApplication, HttpStatus } from '@nestjs/common';
 import { reportByVaultExample } from '../example';
 import { bootstrapTestApp, closeTestApp, vaultAddress, cid } from './reports-http.controller.e2e-setup';
 
-describe('ReportsHttpController (e2e) - getReportByCidAndVault', () => {
+const latestReportDataMock = {
+  timestamp: BigInt(1),
+  refSlot: BigInt(1),
+  treeRoot: '__someTreeRoot__',
+  reportCid: cid,
+};
+
+describe('ReportsHttpController (e2e) - getLast', () => {
   let app: INestApplication;
   let loggerMock;
   let vaultDbServiceMock;
-  let reportsMerkleServiceMock;
+  let lazyOracleMock;
+  let lsvServiceMock;
 
   beforeAll(async () => {
     const bootstrap = await bootstrapTestApp();
     app = bootstrap.app;
 
-    ({ vaultDbServiceMock, reportsMerkleServiceMock, loggerMock } = bootstrap.mocks);
+    ({ vaultDbServiceMock, lazyOracleMock, lsvServiceMock, loggerMock } = bootstrap.mocks);
   });
 
   beforeEach(() => {
     jest.clearAllMocks();
     vaultDbServiceMock.existsVaultByAddress.mockResolvedValue(true);
-    reportsMerkleServiceMock.getReportProofByVault.mockResolvedValue(reportByVaultExample);
+    lazyOracleMock.getLatestReportData.mockResolvedValue(latestReportDataMock);
+    lsvServiceMock.getReportProofByVault.mockResolvedValue(reportByVaultExample);
   });
 
   afterAll(async () => {
@@ -28,64 +37,59 @@ describe('ReportsHttpController (e2e) - getReportByCidAndVault', () => {
   });
 
   it(`${HttpStatus.BAD_REQUEST}: invalid vaultAddress`, async () => {
-    // 1 character is missing
     const badAddress = '0x' + '1'.repeat(39);
-    const resp = await request(app.getHttpServer()).get(`/v1/report/${cid}/${badAddress}`);
+    const resp = await request(app.getHttpServer()).get(`/v1/report/last/${badAddress}`);
     expect(resp.status).toBe(HttpStatus.BAD_REQUEST);
     expect(vaultDbServiceMock.existsVaultByAddress).not.toHaveBeenCalled();
-    expect(reportsMerkleServiceMock.getReportProofByVault).not.toHaveBeenCalled();
-  });
-
-  it(`${HttpStatus.BAD_REQUEST}: invalid cid`, async () => {
-    // too short
-    const badCid = 'Qm' + 'a'.repeat(10);
-    const resp = await request(app.getHttpServer()).get(`/v1/report/${badCid}/${vaultAddress}`);
-    expect(resp.status).toBe(HttpStatus.BAD_REQUEST);
-    expect(vaultDbServiceMock.existsVaultByAddress).not.toHaveBeenCalled();
-    expect(reportsMerkleServiceMock.getReportProofByVault).not.toHaveBeenCalled();
+    expect(lazyOracleMock.getLatestReportData).not.toHaveBeenCalled();
+    expect(lsvServiceMock.getReportProofByVault).not.toHaveBeenCalled();
   });
 
   it(`${HttpStatus.NOT_FOUND}: vault does not exist`, async () => {
     vaultDbServiceMock.existsVaultByAddress.mockResolvedValue(false);
-    const resp = await request(app.getHttpServer()).get(`/v1/report/${cid}/${vaultAddress}`);
+    const resp = await request(app.getHttpServer()).get(`/v1/report/last/${vaultAddress}`);
     expect(resp.status).toBe(HttpStatus.NOT_FOUND);
     expect(vaultDbServiceMock.existsVaultByAddress).toHaveBeenCalledTimes(1);
     expect(vaultDbServiceMock.existsVaultByAddress).toHaveBeenCalledWith(vaultAddress);
-    expect(reportsMerkleServiceMock.getReportProofByVault).not.toHaveBeenCalled();
+    expect(lazyOracleMock.getLatestReportData).not.toHaveBeenCalled();
+    expect(lsvServiceMock.getReportProofByVault).not.toHaveBeenCalled();
   });
 
-  it(`${HttpStatus.OK}: returns vault report`, async () => {
-    const resp = await request(app.getHttpServer()).get(`/v1/report/${cid}/${vaultAddress}`);
+  it(`${HttpStatus.OK}: returns last report`, async () => {
+    const resp = await request(app.getHttpServer()).get(`/v1/report/last/${vaultAddress}`);
     expect(resp.status).toBe(HttpStatus.OK);
     expect(resp.body).toEqual({ report: reportByVaultExample });
     expect(vaultDbServiceMock.existsVaultByAddress).toHaveBeenCalledTimes(1);
     expect(vaultDbServiceMock.existsVaultByAddress).toHaveBeenCalledWith(vaultAddress);
-    expect(reportsMerkleServiceMock.getReportProofByVault).toHaveBeenCalledTimes(1);
-    expect(reportsMerkleServiceMock.getReportProofByVault).toHaveBeenCalledWith(vaultAddress, cid);
+    expect(lazyOracleMock.getLatestReportData).toHaveBeenCalledTimes(1);
+    expect(lsvServiceMock.getReportProofByVault).toHaveBeenCalledTimes(1);
+    expect(lsvServiceMock.getReportProofByVault).toHaveBeenCalledWith(vaultAddress, cid);
   });
 
   it(`${HttpStatus.OK}: report found, vault is not present in report, but controller returns { report: null }`, async () => {
-    reportsMerkleServiceMock.getReportProofByVault.mockRejectedValueOnce(new Error('Vault not found in report'));
-    const resp = await request(app.getHttpServer()).get(`/v1/report/${cid}/${vaultAddress}`);
+    lsvServiceMock.getReportProofByVault.mockRejectedValueOnce(new Error('Vault not found in report'));
+    const resp = await request(app.getHttpServer()).get(`/v1/report/last/${vaultAddress}`);
     expect(resp.status).toBe(HttpStatus.OK);
     expect(resp.body).toEqual({ report: null });
     expect(loggerMock.error).toHaveBeenCalledTimes(1);
     expect(String(loggerMock.error.mock.calls[0][0])).toContain(`Failed to getReportProofByVault ${vaultAddress}`);
     expect(vaultDbServiceMock.existsVaultByAddress).toHaveBeenCalledTimes(1);
     expect(vaultDbServiceMock.existsVaultByAddress).toHaveBeenCalledWith(vaultAddress);
-    expect(reportsMerkleServiceMock.getReportProofByVault).toHaveBeenCalledTimes(1);
-    expect(reportsMerkleServiceMock.getReportProofByVault).toHaveBeenCalledWith(vaultAddress, cid);
+    expect(lazyOracleMock.getLatestReportData).toHaveBeenCalledTimes(1);
+    expect(lsvServiceMock.getReportProofByVault).toHaveBeenCalledTimes(1);
+    expect(lsvServiceMock.getReportProofByVault).toHaveBeenCalledWith(vaultAddress, cid);
   });
 
   it(`${HttpStatus.OK}: report not found, but controller returns { report: null }`, async () => {
-    reportsMerkleServiceMock.getReportProofByVault.mockRejectedValueOnce(new Error('Report not found'));
-    const resp = await request(app.getHttpServer()).get(`/v1/report/${cid}/${vaultAddress}`);
+    lsvServiceMock.getReportProofByVault.mockRejectedValueOnce(new Error('Report not found'));
+    const resp = await request(app.getHttpServer()).get(`/v1/report/last/${vaultAddress}`);
     expect(resp.status).toBe(HttpStatus.OK);
     expect(resp.body).toEqual({ report: null });
     expect(loggerMock.error).toHaveBeenCalledTimes(1);
     expect(vaultDbServiceMock.existsVaultByAddress).toHaveBeenCalledTimes(1);
     expect(vaultDbServiceMock.existsVaultByAddress).toHaveBeenCalledWith(vaultAddress);
-    expect(reportsMerkleServiceMock.getReportProofByVault).toHaveBeenCalledTimes(1);
-    expect(reportsMerkleServiceMock.getReportProofByVault).toHaveBeenCalledWith(vaultAddress, cid);
+    expect(lazyOracleMock.getLatestReportData).toHaveBeenCalledTimes(1);
+    expect(lsvServiceMock.getReportProofByVault).toHaveBeenCalledTimes(1);
+    expect(lsvServiceMock.getReportProofByVault).toHaveBeenCalledWith(vaultAddress, cid);
   });
 });
