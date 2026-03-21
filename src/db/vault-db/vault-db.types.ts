4cf156316fefa2c5b5d9a5895440927a7c3d0ae6
Anton Shalimov <a.shalimof@yandex.ru>
2026-03-21 23:58:03 +0300

    refactor: move APR SMA logic to service and handle anomaly/outdated correctly

--- src/db/vault-db/vault-db.types.ts
+++ src/db/vault-db/vault-db.types.ts
@@ -3,6 +3,7 @@ export type SeriesReportPoint = { reportCid: string; timestamp: number };
 export type AprSma = { aprs: number[]; sma: number };
 
 export type VaultAprSma = {
+  outdated: boolean;
   days: number;
   count: number;
   range: { fromTimestamp: number; toTimestamp: number };
