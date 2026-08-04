64282c771226d405ada6c9c92ef6c9b296e513e8
DiRaiks <andrei.fi@lido.fi>
2026-08-04 16:00:14 +0300

    fix: set DISCONNECTED_VAULTS_OWNERSHIP_CRON to 40 8 * * *

--- src/common/config/env.validation.ts
+++ src/common/config/env.validation.ts
@@ -118,7 +118,7 @@ export class EnvironmentVariables {
 
   @IsOptional()
   @IsString()
-  DISCONNECTED_VAULTS_OWNERSHIP_CRON = '40 3 * * *';
+  DISCONNECTED_VAULTS_OWNERSHIP_CRON = '40 8 * * *';
 
   @IsOptional()
   @IsString()
