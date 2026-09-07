f94fe4389c62784ced85a65c99a2cb61165336b7
Dmitriy Evtifeev <ev.d@lido.fi>
2026-09-07 20:20:22 +0300

    feat: add SkipCache decorator for health endpoint

---
+++ src/http/common/cache/custom-cache.interceptor.ts
@@ -0,0 +1,19 @@
+import { CacheInterceptor } from '@nestjs/cache-manager';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { SKIP_CACHE_KEY } from 'common/decorators/skipCache';
+
+@Injectable()
+export class CustomCacheInterceptor extends CacheInterceptor {
+  protected trackBy(context: ExecutionContext) {
+    const shouldSkip = this.reflector.getAllAndOverride<boolean>(SKIP_CACHE_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (shouldSkip) {
+      return undefined;
+    }
+
+    return super.trackBy(context);
+  }
+}
--- src/http/common/cache/index.ts
+++ src/http/common/cache/index.ts
@@ -1,3 +1,4 @@
 export * from './cache.module';
 export * from './cache-control-headers.interceptor';
 export * from './cache.decorator';
+export * from './custom-cache.interceptor';
