da8ea1e2e858e38932cfe35862fccccf24033b72
Dmitriy Evtifeev <ev.d@lido.fi>
2026-09-08 18:34:29 +0300

    refactor: add todo to health route, add SkipCache and SkipTrottle to LIVE_URL

--- src/common/health/health.constants.ts
+++ src/common/health/health.constants.ts
@@ -1,2 +1,6 @@
+// TODO:
+//  During the migration from the old infrastructure to k8s,
+//  we'll keep the existing /health endpoint for compatibility.
+//  In the future, it will most likely be renamed to /readyz to better reflect its semantics.
 export const HEALTH_URL = 'health';
 export const LIVE_URL = 'livez';
